493b6b874b9804ef5091a7c150157bd1cb1a36a2
Jason Sinn <jrs341@me.com>
2016-10-03 12:56:13 -0500

    added the table and form fields to HTML

--- assets/train.js
+++ assets/train.js
@@ -1,4 +1,16 @@
+ // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyD1PKOz5uBByt0jmnUDdebdc8InEyh0lTU",
+    authDomain: "train-scheduler-f796f.firebaseapp.com",
+    databaseURL: "https://train-scheduler-f796f.firebaseio.com",
+    storageBucket: "",
+    messagingSenderId: "892159836978"
+  };
+  firebase.initializeApp(config);
+
+
+
 $(document).ready(function(){
 
-	
+
 });
