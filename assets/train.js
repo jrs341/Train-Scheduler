4017aba5c757917383295921149b9a6928ed1f53
Jason Sinn <jrs341@me.com>
2016-12-24 17:12:07 -0600

    finally got the minutes away to work

--- assets/train.js
+++ assets/train.js
@@ -12,19 +12,20 @@ $(document).ready(function(){
 
 var database = firebase.database();
 
+// Displays a clock on the page
 function update() {
   $('#clock').html(moment().format('D. MMMM YYYY H:mm:ss'));
 }
 
 setInterval(update, 1000);
 
-// 2. Button for adding Employees
+// 2. Button for adding Trains
 $("#addTrainBtn").on("click", function(){
 
 	// Grabs user input
 	var trainName = $("#trainName").val().trim();
 	var trainDestination = $("#destination").val().trim();
-	var trainTime = moment($("#trainTime").val().trim(), "HH:mm").format();
+	var trainTime = moment($("#trainTime").val().trim(), "HH:mm").subtract(10, "years").format("X");
 	var trainFrequency = $("#frequency").val().trim();
 
 	// Creates local "temporary" object for holding employee data
@@ -35,14 +36,14 @@ $("#addTrainBtn").on("click", function(){
 		frequency: trainFrequency
 	}
 
-	// Uploads employee data to the database
+	// Uploads train data to the database
 	database.ref().push(newTrain);
 
 	// Logs everything to console
-	// console.log(newTrain.name);
-	// console.log(newTrain.destination);
-	// console.log(newTrain.firstTrain);
-	// console.log(newTrain.frequency);
+	console.log(newTrain.name);
+	console.log(newTrain.destination);
+	console.log(newTrain.firstTrain);
+	console.log(newTrain.frequency);
 
 	// Alert
 	alert("Train successfully added");
@@ -58,7 +59,7 @@ $("#addTrainBtn").on("click", function(){
 });
 
 
-// 3. Create Firebase event for adding employee to the database and a row in the html when a user adds an entry
+// 3. Create Firebase event for adding trains to the database and a row in the html when a user adds an entry
 database.ref().on("child_added", function(childSnapshot, prevChildKey){
 
 	console.log(childSnapshot.val());
@@ -66,67 +67,38 @@ database.ref().on("child_added", function(childSnapshot, prevChildKey){
 	// Store everything into a variable.
 	var trainName = childSnapshot.val().name;
 	var destination = childSnapshot.val().destination;
-	var firstTrain = moment(childSnapshot.val().firstTrain);
+	var firstTrain = childSnapshot.val().firstTrain;
 	var trainFrequency = childSnapshot.val().frequency;
-	var nextArrival = firstTrain.clone().add(trainFrequency, 'minutes').format('HH:mm');
+	// var nextArrival = firstTrain.clone().add(trainFrequency, 'minutes').format('HH:mm');
 
-	// var eventTime= 1366549200; // Timestamp - Sun, 21 Apr 2013 13:00:00 GMT
-	// var currentTime = 1366547400; // Timestamp - Sun, 21 Apr 2013 12:30:00 GMT
-	// var diffTime = eventTime - currentTime;
-	// var duration = moment.duration(diffTime*1000, 'milliseconds');
-	// var interval = 1000;
-	// setInterval(function(){
- //  	duration = moment.duration(duration - interval, 'milliseconds');
- //    $('.countdown').text(duration.hours() + ":" + duration.minutes() + ":" + duration.seconds())
-	// }, interval);
-	// var minutesAway = countdown every frequency;
-	// Employee Info
-	console.log(trainName);
-	console.log(destination);
-	console.log(firstTrain);
-	console.log(trainFrequency);
+	// Train Info
+	// console.log(trainName);
+	// console.log(destination);
+	// console.log(firstTrain);
+	// console.log(trainFrequency);
+	// console.log(nextArrival);
+	// console.log(moment());
+	// if (moment() > firstTrain) {
+	// 	console.log('in function');
+	// 	var diff = firstTrain - moment();
+	// 	console.log(moment(diff).format('LLL'));
+	var differenceTimes = moment().diff(moment.unix(firstTrain), "minutes");
+	var remainingTime = moment().diff(moment.unix(firstTrain), "minutes") % trainFrequency;
+	var minutes = trainFrequency - remainingTime;
+	var nextArrival = moment().add(minutes, "m").format("HH:mm");
+	console.log(remainingTime);
+	console.log(minutes);
 	console.log(nextArrival);
-	console.log(moment());
-	if (moment() > firstTrain) {
-		console.log('in function');
-		var diff = firstTrain - moment();
-		console.log(moment(diff).format('LLL'));
 
-	} else {
-		console.log('Train will start running' + firstTrain.format('LLL'));
-	}
-
-	// Prettify the employee start
-	// var empStartPretty = moment.unix(empStart).format("MM/DD/YY");
-	// // Calculate the months worked using hardconre math
-	// // To calculate the months worked
-	// var empMonths = moment().diff(moment.unix(empStart, 'X'), "months");
-	// console.log(empMonths);
-
-	// // Calculate the total billed rate
-	// var empBilled = empMonths * empRate;
-	// console.log(empBilled);
+	// } else {
+	// 	console.log('Train will start running' + firstTrain.format('LLL'));
+	// }
 
 	// Add each train's data into the table
 	// <td>" + minutesAway + "<td>"
-	$("#trainSchedule > tbody").append("<tr><td>" + trainName + "</td><td>" + destination + "</td><td>" + trainFrequency + "</td><td>" + nextArrival + "</td>");
+	$("#trainSchedule > tbody").append("<tr><td>" + trainName + "</td><td>" + destination + "</td><td>" + trainFrequency + "</td><td>" + nextArrival + "</td><td>" + minutes + "<td>");
 // database.ref closing bracket
 });
 
 // document.ready closing bracket
 });
-
-
-// Example Time Math
-// -----------------------------------------------------------------------------
-// Assume Employee start date of January 1, 2015
-// Assume current date is March 1, 2016
-
-// We know that this is 15 months.
-// Now we will create code in moment.js to confirm that any attempt we use mets this test case
-
-
-// $(document).ready(function(){
-
-
-// });
